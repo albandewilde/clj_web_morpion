f0d4dc0d14f86d477210ed060aefe7685311a640
albandewilde <dewilde.alban@gmail.com>
2021-07-02 15:09:29 +0200

    The player can now replace empty place with number 1

--- webmorpion/src/webmorpion/handler.clj
+++ webmorpion/src/webmorpion/handler.clj
@@ -5,16 +5,42 @@
             [selmer.parser :as parser]))
 
 (defn ttt []
-  (parser/render-file "./templates/home.html" {:list ( range 3 )}))
+  (parser/render-file "./templates/home.html" {
+                                               :c00 0, :c01 0, :c02 0,
+                                               :c10 0, :c11 0, :c12 0,
+                                               :c20 0, :c21 0, :c22 0,
+                                               :board "c00=0&c01=0&c02=0&c10=0&c11=0&c12=0&c20=0&c21=0&c22=0"
+                                               }))
 
-(defn play []
+(defn play [params]
   ;; Get the player from the query string
+  (def box (get params "box"))
 
   ;; Get the board from the query string
+  (def board {
+              "c00" (Integer/parseInt (get params "c00")), "c01" (Integer/parseInt (get params "c01")), "c02" (Integer/parseInt (get params "c02")),
+              "c10" (Integer/parseInt (get params "c10")), "c11" (Integer/parseInt (get params "c11")), "c12" (Integer/parseInt (get params "c12")),
+              "c20" (Integer/parseInt (get params "c20")), "c21" (Integer/parseInt (get params "c21")), "c22" (Integer/parseInt (get params "c22")),
+              })
+
+  (def board (assoc board box 1))
+  (def queryBoard (clojure.string/join [
+                                        "c00=" (get board "c00") "&"
+                                        "c01=" (get board "c01") "&"
+                                        "c02=" (get board "c02") "&"
+                                        "c10=" (get board "c10") "&"
+                                        "c11=" (get board "c11") "&"
+                                        "c12=" (get board "c12") "&"
+                                        "c20=" (get board "c20") "&"
+                                        "c21=" (get board "c21") "&"
+                                        "c22=" (get board "c22")]))
 
   ;; Display the modified grid
-
- "The player play his turn")
+  (parser/render-file "./templates/home.html" {
+                                               :c00 (get board "c00"), :c01 (get board "c01"), :c02 (get board "c02"),
+                                               :c10 (get board "c10"), :c11 (get board "c11"), :c12 (get board "c12"),
+                                               :c20 (get board "c20"), :c21 (get board "c21"), :c22 (get board "c22"),
+                                               :board queryBoard}))
 
 (defn endgame [player]
   ;; Get the winner from the query string
@@ -26,7 +52,7 @@
 
 (defroutes app-routes
   (GET "/" [] (ttt))
-  (GET "/play" [] (play))
+  (GET "/play" {params :query-params} (play params))
   (GET "/endgame" {params :query-params} (endgame (get params "p")))
   (route/not-found "Not Found"))
 
